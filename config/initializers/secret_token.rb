79b23aaf169bc6a01c2fb72ce2c758c1aa0af5d0
Olivier <henry.olivier@hotmail.fr>
2013-05-18 16:57:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melba::Application.config.secret_token = 'eb4f7094e409ab82e89d8a24610a645953d4179de7a458e7ad4c1df648f774f323bac5b36013212bbb69a761681ae5a4251e47ac4da360f1fd7b8f5274772cd9'
